9f4c0229e9fc3cb4836f8dc5fdf2beee773be77c
iluxa2003 <ilya.yakov.com@gmail.com>
2022-12-22 15:39:07 +0200

    first steps with react redux && redux toolkit

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,14 +1,21 @@
-import classes from './CartItem.module.css';
-
+import classes from "./CartItem.module.css";
+import { useDispatch } from "react-redux";
+import { quantActions } from "../../storage/storage";
 const CartItem = (props) => {
   const { title, quantity, total, price } = props.item;
-
+  const dispatch = useDispatch();
+  const increaseHandler = () => {
+    dispatch(quantActions.increment());
+  };
+  const decreaseHandler = () => {
+    dispatch(quantActions.decrement());
+  };
   return (
     <li className={classes.item}>
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
+          ${total.toFixed(2)}{" "}
           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
@@ -17,8 +24,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={decreaseHandler}>-</button>
+          <button onClick={increaseHandler}>+</button>
         </div>
       </div>
     </li>
---
+++ src/storage/storage.js
@@ -0,0 +1,19 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+const quantityState = { quantity: 5, isQuantityShown: false };
+
+const quantSlice = createSlice({
+  name: "quant",
+  initialState: quantityState,
+  reducers: {
+    increment(state) {
+      state.quantity = state.quantity + 1;
+    },
+    decrement(state) {
+      state.quantity = state.quantity - 1;
+    },
+  },
+});
+
+const store = configureStore({ reducer: { quant: quantSlice.reducer } });
+export const quantActions = quantSlice.actions;
+export default store;
